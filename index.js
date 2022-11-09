f423a2a1b2b8dc9b241a099260e6df8ed5e291f3
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-11-09 22:35:08 +0800

    write review data to server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { request } = require('express');
 require('dotenv').config();
 
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const collectionOfServices = client.db('smile-bright').collection('services');
+        const collectionOfreview = client.db('smile-bright').collection('review');
         app.get('/home',async(req,res)=>{
             const query = {};
             const cursor = collectionOfServices.find(query).limit(3); 
@@ -31,6 +32,37 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
 
+        })
+
+        app.get('/service-details/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const services = await collectionOfServices.findOne(query); 
+            res.send(services);
+
+        })
+        app.get('/review/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const services = await collectionOfServices.findOne(query); 
+            res.send(services);
+            
+        })
+
+
+        // review API
+        app.get('/display-review/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {serrviceId: id};
+            const services = await collectionOfreview.find(query); 
+            res.send(services);
+
+        })
+        app.post("/review",async(req, res)=>{
+            const review = req.body;            
+            const result = await collectionOfreview.insertOne(review);
+            console.log(result);
+            res.send(result);
         })
         
 
